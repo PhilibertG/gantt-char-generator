db719532dd0dc49c4c592122c3991495c7de1c65
Philibert Gentien <philibert.gentien@epitech.eu>
2025-02-03 14:34:21 +0100

    Adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,14 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Gantt generator',
+  applicationName: 'Gantt Chart Generator',
   description: 'Created by Philibert Gentien',
+  authors: [
+    {
+      name: 'Philibert Gentien',
+      url: "https://philibert-gentien.digitalprojects.fr/",
+    }
+  ]
 }
 
 export default function RootLayout({
